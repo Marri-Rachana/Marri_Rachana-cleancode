191186fc0948e96d762e9f7c48f9192a03c9bcee
Marri-Rachana <56470546+Marri-Rachana@users.noreply.github.com>
2020-02-20 22:28:16 +0530

    Add files via upload

---
+++ main/java/www/rachana/IntrestCalculator/App.java
@@ -0,0 +1,13 @@
+package www.rachana.IntrestCalculator;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
